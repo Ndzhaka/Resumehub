49cf10a902bd76e48e37eeb52749d4084b409ae2
Ndzhaka Mabasa <146018535+Ndzhaka@users.noreply.github.com>
2025-11-16 20:52:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
-}
+  devIndicators: {
+    port: process.env.PORT || 3000, // Default to 3000 if no PORT environment variable is set
+  },
+};
